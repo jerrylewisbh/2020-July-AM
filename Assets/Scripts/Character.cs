dceb1f662e99f5a2aa2293bb7fcd7f878fb8288f
Jerry <jerrylewisbh@gmail.com>
2020-08-06 13:49:05 -0300

    Initial Commit

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Character : MonoBehaviour
+{
+    public float MoveSpeed = 20;
+    public CharacterController characterController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Debug.Log("Start Was Called");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Update was called");
+    }
+}
