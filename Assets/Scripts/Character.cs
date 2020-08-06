c359a09da3f74a41b4fb9f60809de5f793afd4c1
Jerry <jerrylewisbh@gmail.com>
2020-08-06 16:02:50 -0300

    session 6

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class Character : MonoBehaviour
 {
     public float MoveSpeed = 25;
+    public float RotateSpeed = 10;
     public CharacterController characterController;
 
     // Start is called before the first frame update
@@ -18,6 +19,16 @@ public class Character : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Update was called");
+        Debug.Log("Update Was Called");
+
+        //Gets the value of the horizontal axis -> -1 to 1 
+        float horizontalValue = Input.GetAxis("Horizontal");
+        //Rotates around the local Y axis 
+        transform.Rotate(new Vector3(0, horizontalValue * RotateSpeed, 0));
+        //Gets the value of the vertical axis -> -1 to 1 
+
+        float verticalValue = Input.GetAxis("Vertical");
+        //Moves around the local Z axis 
+        characterController.SimpleMove(transform.forward * (MoveSpeed * verticalValue) );
     }
 }
