eb7be43315821073b1f9f699858fe13a7fc49a60
Jerry <jerrylewisbh@gmail.com>
2020-08-21 09:56:53 -0300

    session 10

---
+++ Assets/Scripts/VRHand.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Handness
+{
+    Right,
+    Left
+}
+
+public class VRHand : MonoBehaviour
+{
+    public Handness handness;
+    private string gripButtonName;
+
+    private Animator animator;
+
+    public void Awake()
+    {
+        if (handness == Handness.Right)
+        {
+            gripButtonName = "RightGrip";
+        }
+        else
+        {
+            gripButtonName = "LeftGrip";
+        }
+
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    public void Update()
+    {
+        float gripValue = Input.GetAxis(gripButtonName);
+        
+      // animator.SetBool("gripPressed", true);
+    }
+}
